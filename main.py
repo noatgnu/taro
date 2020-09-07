63277ca33ef5249014412df705d47a4d8ff6d526
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2020-09-08 08:03:27 +1000

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from tornado.httpclient import  AsyncHTTPClient
+from tornado.httpclient import AsyncHTTPClient
 from tornado import gen, web
 from tornado.ioloop import IOLoop
+from tornado.escape import json_decode
 import pandas as pd
 import asyncio
 from parser_handler.parser import BaseParser, NCEPParser, BioMartParser
@@ -11,11 +12,13 @@ define("port", default=8000, type=int, help="Server listening port")
 
 local_database = {"ncEP": None, "smProt": None, "upep": None}
 
+
 @gen.coroutine
 def get_ncEP_async():
     p = NCEPParser("http://www.jianglab.cn/ncEP/browse.html", {"email": "toan.phung@uqconnect.edu.au"})
     response = yield p.parse()
     response.to_csv("ncEP.txt", sep="\t", index=False)
+
 
 @gen.coroutine
 def get_sorfs_async():
@@ -23,12 +26,30 @@ def get_sorfs_async():
     response = yield p.get_all_attributes()
     print(response)
 
+
 @gen.coroutine
 def send_sorfs_query():
-    test_xml = """<!DOCTYPE Query><Query client="true" processor="JSON" limit="-1" header="1"><Dataset name="BioMart" config="Human"><Filter name="human__strand_104" value="1" filter_list=""/><Attribute name="human__sorf_id_104"/><Attribute name="human__CELL_LINE_104"/><Attribute name="human__chr_104"/><Attribute name="human__strand_104"/><Attribute name="human__sorf_begin_104"/><Attribute name="human__sorf_end_104"/><Attribute name="human__spliced_104"/><Attribute name="human__start_parts_104"/><Attribute name="human__stop_parts_104"/><Attribute name="human__sorf_length_104"/><Attribute name="human__start_codon_104"/><Attribute name="human__downstream_gene_distance_104"/><Attribute name="human__upstream_gene_distance_104"/><Attribute name="human__tr_seq_104"/><Attribute name="human__aa_seq_104"/><Attribute name="human__mass_104"/><Attribute name="human__annotation_104"/><Attribute name="human__biotype_104"/><Attribute name="human__RPKM_104"/><Attribute name="human__coverage_104"/><Attribute name="human__coverage_uniformity_104"/><Attribute name="human__exon_overlap_104"/><Attribute name="human__in_frame_coverage_104"/><Attribute name="human__in_frame_104"/><Attribute name="human__pc_exon_overlap_104"/><Attribute name="human__id_104"/><Attribute name="human__Rltm_min_Rchx_104"/><Attribute name="human__FLOSS_104"/><Attribute name="human__classification_104"/><Attribute name="human__orfscore_104"/><Attribute name="human__peak_shift_104"/><Attribute name="human__PhastCon_104"/><Attribute name="human__PhyloP_104"/><Attribute name="human__p_value_104"/></Dataset></Query>"""
+    test_xml = """<!DOCTYPE Query><Query client="true" processor="JSON" limit="-1" header="1"><Dataset name="BioMart" 
+    config="Human"><Filter name="human__strand_104" value="1" filter_list=""/><Attribute 
+    name="human__sorf_id_104"/><Attribute name="human__CELL_LINE_104"/><Attribute name="human__chr_104"/><Attribute 
+    name="human__strand_104"/><Attribute name="human__sorf_begin_104"/><Attribute 
+    name="human__sorf_end_104"/><Attribute name="human__spliced_104"/><Attribute 
+    name="human__start_parts_104"/><Attribute name="human__stop_parts_104"/><Attribute 
+    name="human__sorf_length_104"/><Attribute name="human__start_codon_104"/><Attribute 
+    name="human__downstream_gene_distance_104"/><Attribute name="human__upstream_gene_distance_104"/><Attribute 
+    name="human__tr_seq_104"/><Attribute name="human__aa_seq_104"/><Attribute name="human__mass_104"/><Attribute 
+    name="human__annotation_104"/><Attribute name="human__biotype_104"/><Attribute name="human__RPKM_104"/><Attribute 
+    name="human__coverage_104"/><Attribute name="human__coverage_uniformity_104"/><Attribute 
+    name="human__exon_overlap_104"/><Attribute name="human__in_frame_coverage_104"/><Attribute 
+    name="human__in_frame_104"/><Attribute name="human__pc_exon_overlap_104"/><Attribute 
+    name="human__id_104"/><Attribute name="human__Rltm_min_Rchx_104"/><Attribute name="human__FLOSS_104"/><Attribute 
+    name="human__classification_104"/><Attribute name="human__orfscore_104"/><Attribute 
+    name="human__peak_shift_104"/><Attribute name="human__PhastCon_104"/><Attribute 
+    name="human__PhyloP_104"/><Attribute name="human__p_value_104"/></Dataset></Query>"""
     client = AsyncHTTPClient()
     import urllib.parse
-    response = yield client.fetch("http://biomart.biobix.be/martservice/results", method="POST", body=urllib.parse.urlencode(dict(query=test_xml)))
+    response = yield client.fetch("http://biomart.biobix.be/martservice/results", method="POST",
+                                  body=urllib.parse.urlencode(dict(query=test_xml)))
     print(response.body)
 
 
@@ -48,18 +69,38 @@ class DatabaseHandler(BaseHandler):
         self.database = database
 
     def get(self):
-        self.write(self.database[self.get_argument("db")].to_json(orient="records"))
+        db = self.get_argument("db")
+        df = self.database[db]
+        start_codon = self.get_argument("startCodon")
+        end_codon = self.get_argument("endCodon")
+        peptideSeq = self.get_argument("peptideSeq")
+        if start_codon:
+            if db == "upep":
+                df = df[df["sorfMrna"].str.startswith(start_codon)]
+
+        if end_codon:
+            if db == "upep":
+                df = df[df["sorfMrna"].str.endswith(end_codon)]
+
+        if peptideSeq:
+            if db == "upep":
+                df = df[df["expSeq"].str.contains(peptideSeq) | df["longSeq"].str.contains(peptideSeq)]
+
+        self.write({"database": db, "columns": [{"name": i} for i in df.columns], "data": df.to_json(orient="records")})
+
 
 @gen.coroutine
 def load_local_database():
     local_database["ncEP"] = pd.read_csv("ncEP.txt", sep="\t")
     local_database["smProt"] = pd.read_csv("smProt.txt", sep="\t", encoding='unicode_escape')
+    local_database["smProt"].columns = local_database["smProt"].columns.str.lower()
     upep_db = pd.read_csv("upep.txt", sep="~", encoding='unicode_escape')
     upep_sp = pd.read_csv("upep_sp.txt", sep="~")
     upep_type = pd.read_csv("upep_type.txt", sep="\t")
     upep_db = upep_db.merge(upep_sp, left_on="species_ref_id", right_on="key")
     upep_db = upep_db.merge(upep_type[["key", "name", "description"]], left_on="spep_type_ref_id", right_on="key")
-    column = [i for i in upep_db.columns if i not in ["created", "key", "key_x", "key_y", "species_ref_id", "spep_type_ref_id"]]
+    column = [i for i in upep_db.columns if
+              i not in ["created", "key", "key_x", "key_y", "species_ref_id", "spep_type_ref_id"]]
     local_database["upep"] = upep_db[column]
 
 
@@ -71,6 +112,6 @@ if __name__ == "__main__":
     application = web.Application([
         (r"/api/", DatabaseHandler, dict(database=local_database))
     ])
+    print("Application is listening at port {}. Keep the process open.".format(options.port))
     application.listen(options.port)
     IOLoop.current().start()
-
